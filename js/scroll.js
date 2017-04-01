6826f18053d96128d833244b625dfcdbd2c96a16
bumshakabum <bumcrystlbum@gmail.com>
2017-04-01 10:52:15 -0600

    experimenting with scroll by full page by button

--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,12 @@
-$('#down').on('click', function(){
-    $('html, body').animate({
-        scrollTop: $(".middle").offset().top
-    }, 1000);
-});
-$('#up').on('click', function(){
-    $('html, body').animate({
-        scrollTop: $(".top").offset().top
-    }, 1000);
+$(document).ready(function(){
+    $('#down').on('click', function(){
+        $('html, body').animate({
+            scrollTop: $(".middle").offset().top
+        }, 1000);
+    });
+    $('#up').on('click', function(){
+        $('html, body').animate({
+            scrollTop: $(".top").offset().top
+        }, 1000);
+    });
 });
