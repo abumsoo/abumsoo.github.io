b2b7bf7cc18f1903009913378fb46984793e95c4
Bum Kim <bumcrystlbum@gmail.com>
2020-09-24 12:47:27 -0400

    light theme

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,64 @@
-function swapText() {
-  let hobby = document.querySelector('.slider');
-  hobby.textContent = hobbies[i];
-  i++;
-  if (i === hobbies.length) i = 0;
-}
+const flow = (function () {
+  let _i = 0;
+  let _myhobbies = [
+    "drums",
+    "violin",
+    "Smite",
+    "Rocket League",
+    "Python",
+    "Scala",
+    "functional programming",
+    "JavaScript",
+    "guitar",
+    "typefaces",
+    "Linux",
+    "window managers",
+    "Japanese language",
+    "slacklining",
+    "climbing",
+    "swing dancing",
+    "self improvement",
+    "reading",
+    "journaling",
+    "writing",
+    "3D modeling",
+    "digital art",
+    "drawing",
+    "piano",
+    "typing fast",
+    "StarCraft II",
+    "mechanical keyboards",
+    "animation"
+  ];
 
-let i = 1;
-let hobbies = [
-  "learn", "climb","code", "write", "think", "listen",
-  "laugh", "draw", "sing", "explore hobbies"
-];
+  const init = () => {
+    loop();
+  };
 
-window.setInterval(swapText, 1000);
+  const loop = () => {
+    setTimeout(() => {
+      addText();
+      _i++;
+      if (_i < _myhobbies.length) {
+        loop();
+      }
+    }, 1000);
+  };
+  /*
+   * Add a hobby to the same block every second
+   */
+  const addText = () => {
+      const hobbies = document.querySelector('.hobbies');
+      if (hobbies.textContent === '') {
+        hobbies.textContent += _myhobbies[_i];
+      } else {
+        hobbies.textContent = _myhobbies[_i] + ', '+ hobbies.textContent;
+      }
+  };
+
+  return {
+    init
+  }
+})();
+
+flow.init();
