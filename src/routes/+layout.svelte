7a6e1d93a073c1023eccf3db26ff3eb5a7781931
Bum Kim <bumcrystlbum@gmail.com>
2023-06-07 00:41:47 -0400

    test tailwind theme colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,6 @@
 	import '../app.css';
 </script>
 
-<slot />
+<div class="bg-pg-grey-dark">
+	<slot />
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      'pg-grey-dark': '#323139',
+      'pg-grey-light': '#47464d',
+      'pg-pink': '#bb569b',
+      'pg-blue': '#3788ae',
+      },
+    },
   },
   plugins: [],
 }
