1c230cfceb8af68f4054994399109e4c5f3f12cf
Bum Kim <bumcrystlbum@gmail.com>
2024-07-25 03:12:47 -0400

    removed old svelte files

--- src/routes/blog/+page.svelte
+++
@@ -1,8 +0,0 @@
-<div class="col-span-1 flex flex-col gap-8">
-	<div class="flex flex-col gap-4">
-		<h3 class="text-2xl">Nothing here yet</h3>
-		<div class="card">
-			<!-- <p>hello class</p> -->
-		</div>
-	</div>
-</div>
